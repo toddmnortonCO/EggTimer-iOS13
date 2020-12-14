8fc0eb4a421a85757b8ca37e5f4435efb817f4cc
toddmnortonCO <toddmnorton@live.com>
2020-12-14 11:33:22 -0700

    working on progress bar, found a bug where progressBar.progress doesn't visual properly

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,23 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 11
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft" : 3, "Medium" : 4, "Hard" : 7]
+        
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
         
-        // using Switch statement but more efficient w/ Else If
-        switch hardness {
-            case "Soft" :
-                print(softTime)
-            case "Medium" :
-                print(mediumTime)
-            case "Hard" :
-                print(hardTime)
-            default:
-                print("Error")
-        }
+        timer.invalidate()
+                
+        let hardness = sender.currentTitle!
+        
+        totalTime = eggTimes[hardness]!
+        
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
     }
+        
+        @objc func updateTimer() {
+            if secondsPassed < totalTime {
+                let percentageProgress = secondsPassed / totalTime
+                progressBar.progress = Float(percentageProgress)
+                secondsPassed += 1
+            } else {
+                timer.invalidate()
+                titleLabel.text = "Done!"
+            }
+        }
 }
