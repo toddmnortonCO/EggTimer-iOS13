f5779e0c0a7f86f4b8f92d7f1142b197f929d388
toddmnortonCO <toddmnorton@live.com>
2020-12-16 09:53:42 -0700

    fixed progressBar and updated titleLabel.text

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -27,16 +27,19 @@ class ViewController: UIViewController {
         
         totalTime = eggTimes[hardness]!
         
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
     }
         
         @objc func updateTimer() {
-            if secondsPassed < totalTime {
-                let percentageProgress = secondsPassed / totalTime
-                progressBar.progress = Float(percentageProgress)
+            if secondsPassed <= totalTime {
                 secondsPassed += 1
+                progressBar.progress = Float(secondsPassed) / Float(totalTime)
             } else {
                 timer.invalidate()
                 titleLabel.text = "Done!"
