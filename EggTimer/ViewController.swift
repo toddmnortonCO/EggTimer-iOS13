c155c16d76b843ce3c0f78df6ad254c68de5bb05
toddmnortonCO <toddmnorton@live.com>
2020-12-16 10:51:12 -0700

    added audio alarm into project and done!

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -18,14 +19,14 @@ class ViewController: UIViewController {
     var timer = Timer()
     var totalTime = 0
     var secondsPassed = 0
+    var player: AVAudioPlayer?
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
-                
         let hardness = sender.currentTitle!
-        
         totalTime = eggTimes[hardness]!
+        let player =
         
         progressBar.progress = 0.0
         secondsPassed = 0
@@ -43,6 +44,10 @@ class ViewController: UIViewController {
             } else {
                 timer.invalidate()
                 titleLabel.text = "Done!"
+                
+                let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+                player = try! AVAudioPlayer(contentsOf: url!)
+                player?.play() 
             }
         }
 }
